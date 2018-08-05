675d692bdae68081df95e90508473c8fd3ade235
Roy Miloh <roymi@wix.com>
2018-08-05 16:38:04 +0300

    styling; add deps to package.json

--- packages/create-yoshi-app/templates/fullstack/index.js
+++ packages/create-yoshi-app/templates/fullstack/index.js
@@ -4,9 +4,11 @@ const bootstrap = require('wix-bootstrap-ng');
 const rootDir = process.env.SRC_PATH || './dist/src';
 const getPath = filename => path.join(rootDir, filename);
 const hadronOpts = {
-  staticArtifacts: [{
-    artifactId: 'com.wixpress.{%projectName%}'
-  }]
+  staticArtifacts: [
+    {
+      artifactId: 'com.wixpress.{%projectName%}',
+    },
+  ],
 };
 
 bootstrap()
