4baf0a7bc5fd84ab71e04b2708ac11ace791ffb2
Roy Miloh <roymi@wix.com>
2018-08-05 16:14:09 +0300

    add hadron to fullstack/babel

--- packages/create-yoshi-app/templates/fullstack/src/server.js
+++ packages/create-yoshi-app/templates/fullstack/src/server.js
@@ -1,12 +1,16 @@
+import { readFileSync } from 'fs';
+import path from 'path';
 import 'regenerator-runtime/runtime';
-import wixRunMode from 'wix-run-mode';
 import ejs from 'ejs';
+import wixRunMode from 'wix-run-mode';
 import wixExpressCsrf from 'wix-express-csrf';
 import wixExpressRequireHttps from 'wix-express-require-https';
-import { readFileSync } from 'fs';
+
+const artifactName = '{%projectName%}';
+const artifactId = `com.wixpress.${artifactName}`;
 
 module.exports = (app, context) => {
-  const config = context.config.load('{%projectName%}');
+  const config = context.config.load(artifactName);
   const templatePath = './src/index.ejs';
   const templateFile = readFileSync(templatePath, 'utf8');
   const isProduction = wixRunMode.isProduction();
@@ -14,23 +18,26 @@ module.exports = (app, context) => {
   app.use(wixExpressCsrf());
   app.use(wixExpressRequireHttps);
 
-  app.get('/', (req, res) => {
-    const renderModel = getRenderModel(req);
-    const html = ejs.render(templateFile, renderModel, {
-      cache: isProduction,
-      filename: templatePath,
+  app.get('/', context.hadron.middleware(), (req, res, next) => {
+    const hadron = res.locals.hadron;
+    const renderModel = getRenderModel(req, hadron);
+    const templatePath = path.join(hadron.staticLocalPath(artifactId, './src'), 'index.ejs');
+    ejs.renderFile(templatePath, renderModel, { cache: isProduction }, (err, html) => {
+      err ? next(err) : res.send(html);
     });
-    res.send(html);
   });
 
-  function getRenderModel(req) {
+  function getRenderModel(req, hadron) {
     return {
       locale: req.aspects['web-context'].language,
       basename: req.aspects['web-context'].basename,
       debug:
         req.aspects['web-context'].debug ||
         process.env.NODE_ENV === 'development',
-      clientTopology: config.clientTopology,
+      clientTopology: {
+        ...config.clientTopology,
+        staticsBaseUrl: hadron.staticUrl(artifactId, config.clientTopology.staticsBaseUrl)
+      },
       title: 'Wix Full Stack Project Boilerplate',
     };
   }
